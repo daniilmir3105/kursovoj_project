08dc26bf765a3875710771373d0c004a955a4f3f
daniil mir <miroshnichienko.daniil@gmail.com>
2020-04-30 13:20:25 +0300

    dikki-fuller test work

---
+++ dikki_fuller.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 21:35:33 2020
+
+@author: Daniil 
+"""
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.iolib.table import SimpleTable
+from sklearn.metrics import r2_score
+#import ml_metrics as metrics
+
+path = r'C:\Users\Home\Documents\DANIIL\programming\python\Code\projekts\data_science\models\kursovoj_project\dataset_kurs.csv'
+#path = r'D:\Daniil\programming\kursovoj_project\dataset_kurs.csv'
+data = pd.read_csv(path, encoding='utf-8')
+#print(data.columns)
+
+features = ['Export_of_natural_gas_of_the_Russian_Federation_in_the_t_th_year_in_billion_cubic_meters',
+            'World_natural_gas_production_in_the_t_th_year_in_billion_cubic_meters', 
+            'The_world_price_of_natural_gas_in_the_t_th_year_in_dollars_billion_cubic_meters',
+            'World_oil_production_in_the_t_th_year_in_billion_cubic_meters', 
+            'World_exports_of_shale_gas_in_the_t_th_year_million_cubic_meters', 
+            'World_proven_reserves_of_natural_gas_in_the_t_th_year_in_billion_cubic_meters', 
+            'Worl_demand_for_natural_gas_in_the_t_th_year_in_billion_cubic_meters', 
+            'Employment_in_the_gas_segment_in_million_in_the_t_th_year', 
+            'World_LNG_prices_in_the_t_th_year_in_dollars_billion_cubic_meters', 
+            'Investments_in_the_gas_segment_in_t_th_year_in_billion_dollar', 
+            'World_oil_prices_in_the_t_th_year_in_billion_dollars_barrel', 
+            'World_production_of_shale_gas_in_the_t_th_year_in_billion_cubic_meters']
+    
+for col in features:
+    param = data[col]
+    test = sm.tsa.adfuller(param)
+    print('adf: ', test[0])
+    print('p-value: ', test[1])
+    print('Critical values: ', test[4])
+
+    if test[0]> test[4]['5%']: 
+        print('есть единичные корни, ряд не стационарен')
+    else:
+        print('единичных корней нет, ряд стационарен')
+    
