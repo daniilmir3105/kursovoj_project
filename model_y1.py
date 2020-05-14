6fddd6e32d6255ab3d47f4bfce8e3fb7ddf37e93
unknown <miroshnichienko.daniil@gmail.com>
2020-05-14 19:53:05 +0300

    make my model better new version

--- model_y1.py
+++ model_y1.py
@@ -7,18 +7,8 @@ from sklearn.ensemble import RandomForestRegressor
 path = r'D:\Daniil\programming\kursovoj_project\dataset_kurs.csv'
 data = pd.read_csv(path, encoding='utf-8')
 #print(data.columns)
-features = [#'Export_of_natural_gas_of_the_Russian_Federation_in_the_t_th_year_in_billion_cubic_meters',
-       #'World_natural_gas_production_in_the_t_th_year_in_billion_cubic_meters',
-       #'The_world_price_of_natural_gas_in_the_t_th_year_in_dollars_billion_cubic_meters',
-       #'World_oil_production_in_the_t_th_year_in_billion_cubic_meters',
-       #'World_exports_of_shale_gas_in_the_t_th_year_million_cubic_meters',
-       'World_proven_reserves_of_natural_gas_in_the_t_th_year_in_billion_cubic_meters',
-       #'Worl_demand_for_natural_gas_in_the_t_th_year_in_billion_cubic_meters',
-       #'Employment_in_the_gas_segment_in_million_in_the_t_th_year',
+features = ['World_proven_reserves_of_natural_gas_in_the_t_th_year_in_billion_cubic_meters',
        'World_LNG_prices_in_the_t_th_year_in_dollars_billion_cubic_meters',]
-       #'Investments_in_the_gas_segment_in_t_th_year_in_billion_dollar',
-       #'World_oil_prices_in_the_t_th_year_in_billion_dollars_barrel',
-       #'World_production_of_shale_gas_in_the_t_th_year_in_billion_cubic_meters']
 
 y = data.Export_of_natural_gas_of_the_Russian_Federation_in_the_t_th_year_in_billion_cubic_meters
 #X = data.drop(['Index', 'Year', 'Export_of_natural_gas_of_the_Russian_Federation_in_the_t_th_year_in_billion_cubic_meters'], axis=1)
--- model_y5.py
+++ model_y5.py
@@ -9,18 +9,9 @@ data = pd.read_csv(path, encoding='utf-8')
 #print(data.columns)
 
 y = data.The_world_price_of_natural_gas_in_the_t_th_year_in_dollars_billion_cubic_meters
-features = [#'Export_of_natural_gas_of_the_Russian_Federation_in_the_t_th_year_in_billion_cubic_meters',
-       #'World_natural_gas_production_in_the_t_th_year_in_billion_cubic_meters',
-       #'The_world_price_of_natural_gas_in_the_t_th_year_in_dollars_billion_cubic_meters',
-       #'World_oil_production_in_the_t_th_year_in_billion_cubic_meters',
-       #'World_exports_of_shale_gas_in_the_t_th_year_million_cubic_meters',
-       #'World_proven_reserves_of_natural_gas_in_the_t_th_year_in_billion_cubic_meters',
-       #'Worl_demand_for_natural_gas_in_the_t_th_year_in_billion_cubic_meters',
-       #'Employment_in_the_gas_segment_in_million_in_the_t_th_year',
-       'World_LNG_prices_in_the_t_th_year_in_dollars_billion_cubic_meters',
-       #'Investments_in_the_gas_segment_in_t_th_year_in_billion_dollar',
-       #'World_oil_prices_in_the_t_th_year_in_billion_dollars_barrel',
+features = ['World_LNG_prices_in_the_t_th_year_in_dollars_billion_cubic_meters',
        'World_production_of_shale_gas_in_the_t_th_year_in_billion_cubic_meters']
+
 #X = data.drop(['Index', 'Year', 'The_world_price_of_natural_gas_in_the_t_th_year_in_dollars_billion_cubic_meters'], axis=1)
 X = data[features]
 
